3fa669a1c137cf8c7ce91abbd5f1cc9209c51cc1
Martin Thuresson <martin.thure@gmail.com>
2014-01-28 18:56:50 +0100

    update url

--- snalltaget.py
+++ snalltaget.py
@@ -1,41 +1,50 @@
 import requests
 import re
 import json
+import tornado.ioloop
+import tornado.web
 
-r = requests.get('https://boka.snalltaget.se/boka-biljett')
-cookie = r.cookies["Token"]
-cookies = dict(Token=cookie)
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+		r = requests.get('https://boka.snalltaget.se/boka-biljett')
+		cookie = r.cookies["Token"]
+		cookies = dict(Token=cookie)
 
-query = json.loads('{"DepartureLocationId":1,"DepartureLocationProducerCode":74,"ArrivalLocationId":110,"ArrivalLocationProducerCode":74,"DepartureDateTime":"2014-02-21 12:00","TravelType":"E","Passengers":[{"PassengerCategory":"VU"}]}')
+		query = json.loads('{"DepartureLocationId":1,"DepartureLocationProducerCode":74,"ArrivalLocationId":110,"ArrivalLocationProducerCode":74,"DepartureDateTime":"2014-02-21 12:00","TravelType":"E","Passengers":[{"PassengerCategory":"VU"}]}')
 
-headers = {'content-type': 'application/json'}
+		headers = {'content-type': 'application/json'}
 
-r = requests.post('https://boka.snalltaget.se/api/timetables', data=json.dumps(query), headers=headers, cookies=cookies)
+		r = requests.post('https://boka.snalltaget.se/api/timetables', data=json.dumps(query), headers=headers, cookies=cookies)
 
-trips = r.json()
+		trips = r.json()
 
-query = {'TimetableId':trips['Id'], 'JourneyConnectionReferences':[]}
+		query = {'TimetableId':trips['Id'], 'JourneyConnectionReferences':[]}
 
-if len(trips['JourneyAdvices']) > 10:
-	max = 10
-else:
-	max = len(trips['JourneyAdvices'])
+		if len(trips['JourneyAdvices']) > 10:
+			max = 10
+		else:
+			max = len(trips['JourneyAdvices'])
 
-for i in range(0, max):
-   query['JourneyConnectionReferences'].append(trips['JourneyAdvices'][i]['JourneyConnectionReference'])
+		for i in range(0, max):
+		   query['JourneyConnectionReferences'].append(trips['JourneyAdvices'][i]['JourneyConnectionReference'])
 
 
-r = requests.post('https://boka.snalltaget.se/api/journeyadvices/lowestprices', data=json.dumps(query), headers=headers, cookies=cookies)
+		r = requests.post('https://boka.snalltaget.se/api/journeyadvices/lowestprices', data=json.dumps(query), headers=headers, cookies=cookies)
 
-price = r.json()
+		price = r.json()
 
-for i in range(0, len(trips['JourneyAdvices'])):
-	for j in range (0,len(price)):
-		if price[j]['JourneyConnectionReference'] == trips['JourneyAdvices'][i]['JourneyConnectionReference']:
-			trips['JourneyAdvices'][i]['IsSleeperTrain'] = price[j]['IsSleeperTrain']
-			trips['JourneyAdvices'][i]['LowestTotalPrice'] = price[j]['LowestTotalPrice']
-			trips['JourneyAdvices'][i]['Currency'] = price[j]['Currency']
-			break
+		for i in range(0, len(trips['JourneyAdvices'])):
+			for j in range (0,len(price)):
+				if price[j]['JourneyConnectionReference'] == trips['JourneyAdvices'][i]['JourneyConnectionReference']:
+					trips['JourneyAdvices'][i]['IsSleeperTrain'] = price[j]['IsSleeperTrain']
+					trips['JourneyAdvices'][i]['LowestTotalPrice'] = price[j]['LowestTotalPrice']
+					trips['JourneyAdvices'][i]['Currency'] = price[j]['Currency']
+					break
+		self.write(trips)
 
-print json.dumps(trips)
-   
+application = tornado.web.Application([
+    (r"/snalltaget/", MainHandler),
+])
+
+application.listen(8888)
+tornado.ioloop.IOLoop.instance().start()
