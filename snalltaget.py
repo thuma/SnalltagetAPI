6e48f2f79e6fd93084b8bc216c27f36d56cf7102
Martin Thuresson <martin.thure@gmail.com>
2014-01-30 20:19:48 +0100

    added search function

--- snalltaget.py
+++ snalltaget.py
@@ -1,3 +1,6 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import requests
 import re
 import json
@@ -13,26 +16,85 @@ class MainHandler(tornado.web.RequestHandler):
 		Thread(target=self.makerequest).start()
 		
 	def returnrequest(self, data):
-		tornado.ioloop.IOLoop.instance().add_callback(self.returndata, data)
+		outdata = {"travelerAge":35,
+		"travelerIsStudent":False,
+		"price":"101",
+		"currency":"SEK",
+		"validPrice":True,
+		"url":"https://boka.snalltaget.se/boka-biljett","sellername":'Snälltåget'}
+		
+		outdata['departureTime'] = data['DepartureDateTime'][11:16]
+		outdata['arrivalTime'] = data['ArrivalDateTime'][11:16]
+		outdata['date'] = data['DepartureDateTime'][:10]
+		outdata['from'] = data['DepartureLocation']['LocationNameShort']
+		outdata['to'] = data['ArrivalLocation']['LocationNameShort']
+		outdata['price'] = int(data['LowestTotalPrice'])
+		outdata['currency'] = data['Currency']
+		
+		tornado.ioloop.IOLoop.instance().add_callback(self.returndata, outdata)
+	
+	def returnerror(self, data):
+		returdata = {}
+		returdata['error'] = data
+		tornado.ioloop.IOLoop.instance().add_callback(self.returndata, returdata)
 	
 	def makerequest(self):
 		global cache
 		r = requests.get('https://boka.snalltaget.se/boka-biljett')
 		cookie = r.cookies["Token"]
 		cookies = dict(Token=cookie)
-
-		query = json.loads('{"DepartureLocationId":1,"DepartureLocationProducerCode":74,"ArrivalLocationId":110,"ArrivalLocationProducerCode":74,"DepartureDateTime":"2014-02-21 12:00","TravelType":"E","Passengers":[{"PassengerCategory":"VU"}]}')
-		#query['DepartureDateTime'] = self.get_argument('date','2014-02-20') + self.get_argument('departureTime','12:00')
+		
+		query = {}
+		
+		#query = json.loads('{"DepartureLocationId":1,"DepartureLocationProducerCode":74,"ArrivalLocationId":110,"ArrivalLocationProducerCode":74,"DepartureDateTime":"2014-02-21 12:00","TravelType":"E","Passengers":[{"PassengerCategory":"VU"}]}')
+		
+		try:
+			query['DepartureDateTime'] = self.get_argument('date') +' '+ self.get_argument('departureTime')
+			getdate = self.get_argument('date')
+			gettime = self.get_argument('departureTime')
+		except:
+			self.returnerror('Missing deparature time')
+			return ''
+			
+		try:
+			getfrom = self.get_argument('from')
+			query['DepartureLocationId'] = int(getfrom[-5:])
+			query['DepartureLocationProducerCode'] = int(getfrom[:2])
+		except:
+			self.returnerror('Missing deparature station')
+			return ''
+		
+		try:
+			getto = self.get_argument('to')
+			query['ArrivalLocationId'] = int(getto[-5:])
+			query['ArrivalLocationProducerCode'] = int(getto[:2])
+		except:
+			self.returnerror('Missing arrival station')
+			return ''
+			
+		query['TravelType'] = "E"
+		query['Passengers'] = [{"PassengerCategory":"VU"}]
+		
+		try:
+			self.returnrequest(cache[getfrom+getdate+gettime+getto])
+			return ''
+		except:
+			notfound = 1
+			
 		headers = {'content-type': 'application/json'}
 
 		r = requests.post('https://boka.snalltaget.se/api/timetables', data=json.dumps(query), headers=headers, cookies=cookies)
 
 		trips = r.json()
 		
-		if len(trips['JourneyAdvices']) > 10:
-			max = 10
-		else:
-			max = len(trips['JourneyAdvices']-1)
+		try:
+			if len(trips['JourneyAdvices']) > 10:
+				max = 10
+			else:
+				max = len(trips['JourneyAdvices']-1)
+		except:
+			self.returnerror('No trip found')
+			return ''
 			
 		pquery = {'TimetableId':trips['Id'], 'JourneyConnectionReferences':[]}
 
@@ -51,12 +113,17 @@ class MainHandler(tornado.web.RequestHandler):
 					trips['JourneyAdvices'][i]['Currency'] = price[j]['Currency']
 					stopfrom = str(trips['JourneyAdvices'][i]['DepartureLocation']['ProducerCode']*100000 + trips['JourneyAdvices'][i]['DepartureLocation']['LocationId'])
 					stopto = str(trips['JourneyAdvices'][i]['ArrivalLocation']['ProducerCode']*100000 +trips['JourneyAdvices'][i]['ArrivalLocation']['LocationId'])
-					timefrom = trips['JourneyAdvices'][i]['DepartureDateTime']
-					timeto = trips['JourneyAdvices'][i]['ArrivalDateTime']
-					cache[stopfrom+timefrom+stopto+timeto] = trips['JourneyAdvices'][i]
+					datefrom = trips['JourneyAdvices'][i]['DepartureDateTime'][:10]
+					timefrom = trips['JourneyAdvices'][i]['DepartureDateTime'][11:16]
+					trips['JourneyAdvices'][i]['ArrivalDateTime']
+					cache[stopfrom+datefrom+timefrom+stopto] = trips['JourneyAdvices'][i]
 					break
-		print cache
-		self.returnrequest(trips)
+		try:
+			self.returnrequest(cache[getfrom+getdate+gettime+getto])
+			return ''
+		except:
+			self.returnerror('Trip not found in search')
+			return ''
 		
 	def returndata(self, trips):
 		self.write(trips)
